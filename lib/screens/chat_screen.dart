2c7056eafe12e7de9f52122ad9ec64f1d3b17478
King-Lota <lottanna47@gmail.com>
2023-03-14 23:48:21 +0100

    IS-LIKED FUNCTION ADDED

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,23 +13,57 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
-    return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-        margin: isMe
-            ? const EdgeInsets.only(top: 8, bottom: 8, left: 80)
-            : const EdgeInsets.only(top: 8, bottom: 8, right: 80),
-        decoration: BoxDecoration(
-            color: isMe
-                ? Theme.of(context).secondaryHeaderColor
-                : const Color(0xFFFFEFEE),
-            borderRadius: isMe
-                ? const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                  )
-                : const BorderRadius.only(
-                    topRight: Radius.circular(15),
-                  )),
-        child: Text(message.text));
+    return Row(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width * 0.75,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+            margin: isMe
+                ? const EdgeInsets.only(top: 8, bottom: 8, left: 80)
+                : const EdgeInsets.only(top: 8, bottom: 8),
+            decoration: BoxDecoration(
+                color: isMe
+                    ? Theme.of(context).secondaryHeaderColor
+                    : const Color(0xFFFFEFEE),
+                borderRadius: isMe
+                    ? const BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                      )
+                    : const BorderRadius.only(
+                        topRight: Radius.circular(15),
+                      )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  message.time,
+                  style: const TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  message.text,
+                  style: const TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            )
+        ),
+        !isMe ? IconButton(
+          onPressed: () {},
+          icon: message.isLiked ? const Icon(Icons.favorite):
+          const Icon(Icons.favorite_border),
+          iconSize: 30,
+          color: message.isLiked
+          ? Theme.of(context).primaryColor
+          : Colors.blueGrey,
+        ) : const Text(''),
+      ],
+    );
   }
 
   @override
@@ -67,7 +101,8 @@ class _ChatScreenState extends State<ChatScreen> {
                       topRight: Radius.circular(30),
                       topLeft: Radius.circular(30)),
                   child: ListView.builder(
-                      padding: EdgeInsets.only(
+                    reverse: true,
+                      padding: const EdgeInsets.only(
                         top: 15,
                       ),
                       itemCount: chats.length,
